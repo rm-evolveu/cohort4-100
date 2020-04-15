810c9e1e5ae9e367ad0ca4b921a675ec4777cd81
Roman Mirakhmedov <rm.evolveu@gmail.com>
2020-04-15 14:48:28 -0600

    END of 100C

--- playground/playground/taxBrackets.js
+++ playground/playground/taxBrackets.js
@@ -1,4 +1,4 @@
 const taxRates = [0.15, 0.205, 0.26, 0.29, 0.33]
-const incomeBases = [0, 48535, 97069, 150473, 214368, Infinity ]
+const incomeBases = [0, 48535, 97069, 150473, 214368, Infinity]
 
 export { taxRates, incomeBases }
